c9e13466a8808c0a464d3076ca17aa6ab897ddfb
Ramon Torres <raymondjavaxx@gmail.com>
2021-12-05 16:15:26 -0500

    Update ESLint (#17)

--- src/Stack.ts
+++ src/Stack.ts
@@ -2,25 +2,25 @@
  * Simple stack data structure.
  */
 export default class Stack<Type> {
-    private readonly items: Type[]
+  private readonly items: Type[];
 
-    constructor() {
-      this.items = [];
+  constructor() {
+    this.items = [];
+  }
+
+  push(item: Type): void {
+    this.items.push(item);
+  }
+
+  pop(): Type | undefined {
+    return this.items.pop();
+  }
+
+  top(): Type | undefined {
+    if (this.items.length === 0) {
+      return undefined;
     }
 
-    push(item: Type): void {
-      this.items.push(item);
-    }
-
-    pop(): Type | undefined {
-      return this.items.pop();
-    }
-
-    top(): Type | undefined {
-      if (this.items.length === 0) {
-        return undefined;
-      }
-
-      return this.items[this.items.length - 1];
-    }
+    return this.items[this.items.length - 1];
+  }
 }
