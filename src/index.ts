f051cf090b75185abafd516061a575eb71d8f5f4
Ramon Torres <raymondjavaxx@gmail.com>
2022-06-12 14:42:15 -0400

    Remove Parser class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
+import parse  from "./parse";
 import render from "./render";
-import Parser from "./Parser";
-
-const parser = new Parser();
 
 /**
  * Convert text to HTML.
@@ -9,7 +7,7 @@ const parser = new Parser();
  * @returns HTML result.
  */
 export default function markly(text: string): string {
-  const ast = parser.parse(text);
+  const ast = parse(text);
   const result = render(ast.children);
   return result.trim();
 }
